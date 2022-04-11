830fa895b0334c104bfd9ede98d698b1720138f6
seabass-08 <49072973+seabass-08@users.noreply.github.com>
2022-04-11 19:46:04 -0400

    Add java code of JDBC queries
    Added java code to execute JDBC queries, changed structure of codebase

---
+++ jdbc/JDBCHelper.java
@@ -0,0 +1,65 @@
+import java.sql.*;
+
+public class JDBCHelper {
+
+    public ResultSet listDentists(Connection conn){
+        String sqlQ = "SELECT userid FROM employee E WHERE E.employeetype = \"Dentist\" GROUP BY E.branch_id"; 
+        PreparedStatement myStatement = conn.prepareStatement(sqlQ);
+        return myStatement.executeQuery();
+    }
+
+    public ResultSet addPatient(Connection conn, String insurance, String email, String record_id, String user_id){
+        String sqlQ = "INSERT INTO patient(insurance, email, record_id, user_id) VALUES (\"" + insurance +"\",\""+ email +"\",\""+ record_id + "\",\""+user_id +"\")";
+        PreparedStatement myStatement = conn.prepareStatement(sqlQ);
+        return myStatement.executeQuery();
+    }
+
+    //needs to be updated after dentist column is added to appointment table
+    public ResultSet checkAppointment(Connection conn){
+        String sqlQ = "SELECT appointment_id FROM appointment A";
+        PreparedStatement myStatement = conn.prepareStatement(sqlQ);
+        return myStatement.executeQuery();
+    }
+
+    //
+    public ResultSet setAppointment(Connection conn, String appointment_id, String a_date, String start_time, String end_time, String appointment_type, String status, int fee_id, int treatment_code){
+        String sqlQ = "INSERT INTO appointment(appointment_id, a_date, start_time, end_time, appointment_type, status, fee_id, treatment_code)";
+        sqlQ = sqlQ + "VALUES (\""+ appointment_id +"\",\""+ a_date +"\",\""+ start_time +" \",\""+ end_time +"\",\""+ appointment_type+"\",\""+ status +"\",\""+ fee_id +"\",\""+treatment_code+"\")";
+        PreparedStatement myStatement = conn.prepareStatement(sqlQ);
+        return myStatement.executeQuery();
+    }
+
+    public ResultSet addEmployee(Connection conn, String employeerole, String employeetype, int salary, int user_id, int branch_id){
+        String sqlQ = "INSERT INTO employee(employeerole, employeetype, salary, user_id, branch_id)";
+        sqlQ = sqlQ + "VALUES (\""+ employeerole +"\",\""+ employeetype +"\",\""+ salary +"\",\""+ user_id +"\",\""+ branch_id +"\");";
+        PreparedStatement myStatement = conn.prepareStatement(sqlQ);
+        return myStatement.executeQuery();
+    }
+
+    public ResultSet checkProcedures(Connection conn){
+        String sqlQ = "SELECT procedure_type FROM appointment_procedure AP";
+        PreparedStatement myStatement = conn.prepareStatement(sqlQ);
+        return myStatement.executeQuery();
+    }
+
+    public static void main(String[] args){
+        // auto close connection and preparedStatement
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgres://srsqrkamklhsii:f01068b4fd889f7cd0d23b191f5eeb9729674ee40dce0a5b7063342aacd94a47@ec2-18-215-96-22.compute-1.amazonaws.com:5432/daek7g9mrllna4", 
+                "srsqrkamklhsii", 
+                "f01068b4fd889f7cd0d23b191f5eeb9729674ee40dce0a5b7063342aacd94a47")) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
